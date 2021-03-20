1f25aa09fdbf288d36cc16349bb09b2a237e25f3
Yiğit Alp <73557550+rixqw@users.noreply.github.com>
2021-03-20 17:03:20 +0300

    Update ramkullanimi.bat

--- ramkullanimi.bat
+++ ramkullanimi.bat
@@ -3,5 +3,5 @@ title Ram Kullanimi
 tasklist /fi "memusage gt 100000">%userprofile%\desktop\"Ram Kullanan Programlar".txt 
 echo Ram Kullanim Bilgileriniz Masaustunuze Yuklenmistir!
 timeout 2 > nul
-echo Bu Bat Dosyasi Phoenix Com. Sirketine Aittir
+echo Bu Bat Dosyasi Nether Company Sirketine Aittir
 exit
