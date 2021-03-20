5f8a10677acea34393b3084019aad9913383becc
Yiğit Alp <73557550+rixqw@users.noreply.github.com>
2021-03-20 17:03:36 +0300

    Update sistembilgi.bat

--- sistembilgi.bat
+++ sistembilgi.bat
@@ -3,5 +3,5 @@ title Sistem Bilgi Ogrenme
 systeminfo>%userprofile%\desktop\"Sistem Bilgilerim".txt
 echo Donanim ve Sistem Bilgileriniz Masaustunuze Yuklenmistir!
 timeout 2 > nul 
-echo Bu Bat Dosyasi Phoenix Com. Sirketine Aittir
+echo Bu Bat Dosyasi Nether Company Sirketine Aittir
 exit
